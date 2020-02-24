3a186a50096e8c0ff0f797f62e1e6d2f667def39
Anh Trung <trunganh253@gmail.com>
2020-02-25 00:32:08 +0200

    Jest testing snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { render } from "@testing-library/react";
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App render snapshot test', () => {
+  test('snapshot renders', () => {
+    const component = renderer.create(<App />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    // re-rendering
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
+
 
